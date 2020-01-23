dcf41cc4da7a1f7350c83a953eec90ef7f1f135c
magictomagic <59344440+magictomagic@users.noreply.github.com>
2020-01-24 01:48:32 +0800

    Add files via upload

---
+++ IECS-Attendance/main2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+begin_time=$(date +%s)
+
+rawdata=$(face_recognition --tolerance 0.406 --cpus 4 /home/hhy/IECS-Attendance/101individuals/ /home/hhy/IECS-Attendance/101sum1/|egrep "[0-9]{10}$"|cut -f 2 -d ',')
+	mkdir tmp
+	cat > tmp/rawdata.txt <<-EOF
+	$rawdata
+EOF
+	data=$(sort tmp/rawdata.txt|uniq)
+	cat > tmp/data.txt <<-EOF
+	$data
+EOF
+	num=$(grep -c "" tmp/data.txt)
+	rm -rf tmp
+
+echo "${data}"
+echo "a total of ${num} people"
+# bottom
+end_time=$(date +%s)
+run_time=$((end_time-begin_time))
+echo "run time: ${run_time}s"
